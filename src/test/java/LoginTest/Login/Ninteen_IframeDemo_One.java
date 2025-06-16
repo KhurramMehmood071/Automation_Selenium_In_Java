09b45bc433a120fcf1bba4f8382bdb4f7a43fb58
KhurramMehmood071 <khurram071@outlook.com>
2025-06-16 14:23:15 +0500

    Nested Iframe

--- src/test/java/LoginTest/Login/Ninteen_IframeDemo_One.java
+++ src/test/java/LoginTest/Login/Ninteen_IframeDemo_One.java
@@ -4,29 +4,28 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-
 public class Ninteen_IframeDemo_One {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		//Launch chrome browser
-				System.setProperty("webdriver.chrome.driver", "C:\\Users\\prach\\Desktop\\DemoTestMavenProject\\Test1\\Drivers\\chromedriver.exe");
-				WebDriver driver = new ChromeDriver();
 
-				//maximise browser
-				driver.manage().window().maximize();
-				//open url
-				driver.get("https://www.rediff.com/");
-				
-				//switch to iframe moneyiframe
-				driver.switchTo().frame("moneyiframe");
-				
-				//find web element & print value
-				String nseindex = driver.findElement(By.id("nseindex")).getText();
+		// Launch chrome browser
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\AKDNdHRC\\Selenium\\Training Sessions\\Session 01\\MavenLoginTest\\Login\\chromedriver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
 
-				System.out.println(nseindex);
+		// maximise browser
+		driver.manage().window().maximize();
+		// open url
+		driver.get("https://www.rediff.com/");
 
+		// switch to iframe moneyiframe
+		driver.switchTo().frame("moneyiframe");
+
+		// find web element & print value
+		String nseindex = driver.findElement(By.id("nseindex")).getText();
+
+		System.out.println(nseindex);
 
 	}
 
--- src/test/java/LoginTest/Login/Twenty_Iframe_Demo_Two.java
+++ src/test/java/LoginTest/Login/Twenty_Iframe_Demo_Two.java
@@ -9,37 +9,38 @@ public class Twenty_Iframe_Demo_Two {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		//Launch chrome browser
-				System.setProperty("webdriver.chrome.driver", "C:\\Users\\prach\\Desktop\\DemoTestMavenProject\\Test1\\Drivers\\chromedriver.exe");
-				WebDriver driver = new ChromeDriver();
 
-				//maximise browser
-				driver.manage().window().maximize();
-				//open url
-				driver.get("https://www.selenium.dev/selenium/docs/api/java/index.html?overview-summary.html");
-				
-				//switch to 1st frame
-				WebElement iframeElement = driver.findElement(By.name("packageListFrame"));
-				driver.switchTo().frame(iframeElement);
-				
-				//find web element
-				driver.findElement(By.linkText("org.openqa.selenium")).click();
-				
-				//switch to main page
-				driver.switchTo().defaultContent();//main page
-				
-				//swith to 2nd frame
-				driver.switchTo().frame(1);
-				//find web element "Alert" Link text of 2nd frame
-				driver.findElement(By.linkText("Alert")).click();
-				
-				//switch to main page
-				driver.switchTo().defaultContent();//main page
-				
-				//find & print total iframes on web page
-				int noOfiframes = driver.findElements(By.tagName("iframe")).size();
-				System.out.println("no. of iframes:" + noOfiframes);
+		// Launch chrome browser
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\AKDNdHRC\\Selenium\\Training Sessions\\Session 01\\MavenLoginTest\\Login\\chromedriver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+
+		// maximise browser
+		driver.manage().window().maximize();
+		// open url
+		driver.get("https://www.selenium.dev/selenium/docs/api/java/index.html?overview-summary.html");
+
+		// switch to 1st frame
+		WebElement iframeElement = driver.findElement(By.name("packageListFrame"));
+		driver.switchTo().frame(iframeElement);
+
+		// find web element
+		driver.findElement(By.linkText("org.openqa.selenium")).click();
+
+		// switch to main page
+		driver.switchTo().defaultContent();// main page
+
+		// swith to 2nd frame
+		driver.switchTo().frame(1);
+		// find web element "Alert" Link text of 2nd frame
+		driver.findElement(By.linkText("Alert")).click();
+
+		// switch to main page
+		driver.switchTo().defaultContent();// main page
+
+		// find & print total iframes on web page
+		int noOfiframes = driver.findElements(By.tagName("iframe")).size();
+		System.out.println("no. of iframes:" + noOfiframes);
 
 	}
 
