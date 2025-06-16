f679da1f2934432b607bb61f35f9567ba9763473
KhurramMehmood071 <khurram071@outlook.com>
2025-06-16 13:56:22 +0500

    Create New Classess

---
+++ src/test/java/LoginTest/Login/Ninteen_IframeDemo_One.java
@@ -0,0 +1,33 @@
+package LoginTest.Login;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class Ninteen_IframeDemo_One {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Launch chrome browser
+				System.setProperty("webdriver.chrome.driver", "C:\\Users\\prach\\Desktop\\DemoTestMavenProject\\Test1\\Drivers\\chromedriver.exe");
+				WebDriver driver = new ChromeDriver();
+
+				//maximise browser
+				driver.manage().window().maximize();
+				//open url
+				driver.get("https://www.rediff.com/");
+				
+				//switch to iframe moneyiframe
+				driver.switchTo().frame("moneyiframe");
+				
+				//find web element & print value
+				String nseindex = driver.findElement(By.id("nseindex")).getText();
+
+				System.out.println(nseindex);
+
+
+	}
+
+}
